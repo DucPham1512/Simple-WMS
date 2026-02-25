3a7205ae2fe529a953d0ab2fa09d42b48991f4f8
DucPham1512 <duc.phamtan@hcmut.edu.vn>
2026-02-25 11:45:58 +0700

    Add Supplier page and DI registration
    Added "Nhà cung cấp" (Supplier) page to app shell navigation. Registered NhaCungCapPage, its page model, and repository in DI container. Updated project file to include and compile NhaCungCapPage.xaml and code-behind.

--- InternProj/MauiProgram.cs
+++ InternProj/MauiProgram.cs
@@ -30,6 +30,9 @@ namespace InternProj
             builder.Services.AddSingleton<SanPhamRepository>();
             builder.Services.AddTransient<SanPhamPageModel>();
             builder.Services.AddTransient<SanPhamPage>();
+            builder.Services.AddSingleton<NhaCungCapRepository>();
+            builder.Services.AddTransient<NhaCungCapPageModel>();
+            builder.Services.AddTransient<NhaCungCapPage>();
 #endif
 
             return builder.Build();
